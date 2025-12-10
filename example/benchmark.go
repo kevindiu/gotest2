80cda38f4936a48b08ac4060827638459d4d5031
kevindiu <kevindiujp@gmail.com>
2025-12-10 01:19:41 +0000

    fix

--- example/benchmark.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "time"
-
-// SlowFunction sleeps for a duration.
-func SlowFunction(d time.Duration) {
-	time.Sleep(d)
-}
--- example/benchmark_only.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func Multiply(a, b int) int {
-	return a * b
-}
